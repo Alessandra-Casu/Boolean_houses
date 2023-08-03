bb11d944b6113e4ae4a2eb6752d53b8113a446ca
Alessandra-Casu <alessandracasu919@gmail.com>
2023-08-03 11:53:51 +0200

    Fourth commit: add controller and views (index)

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers\Guest;
 
-use App\Http\Controllers\Controller;
+use App\Models\House;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PageController extends Controller
 {
-    //
+    public function index() {
+        $houses = House::all();
+
+        return view('index', [
+            'houses' => $houses,
+        ]);
+    }
 }
